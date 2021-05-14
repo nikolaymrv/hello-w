1bb758eadb9f066934c67de4f57dcdf33e3810ea
YaSnegir <nm6023648@gmail.com>
2021-05-14 19:05:16 +0300

    commit this

---
+++ edu.mykola.maven.eclipse/src/main/java/edu/mykola/edu/mykola/maven/eclipse/App.java
@@ -0,0 +1,17 @@
+package edu.mykola.edu.mykola.maven.eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+   
+  //1. create project with gradle
+    //2. commit and push (maven + gralde) code to github (tutorial: https://www.atlassian.com/git/tutorials/learn-git-with-bitbucket-cloud)
+
+}
